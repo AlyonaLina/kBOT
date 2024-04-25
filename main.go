7975f8d4ab94bf92d006112321b190689c296424
AlyonaLina <168096029+AlyonaLina@users.noreply.github.com>
2024-04-25 22:41:57 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import (
+	"log"
+	"time"
+    "os"
+	tele "gopkg.in/telebot.v3"
+)
+
+
+func main() {
+	pref := tele.Settings{
+		Token:  os.Getenv("TELE_TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	b.Start()
+}
